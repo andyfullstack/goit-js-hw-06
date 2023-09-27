415064b99fdc557ae7030b9d814ca6a9bbaadb61
andyfullstack <andriikoliadenko@gmail.com>
2023-09-27 18:04:15 +0300

    upd

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,12 @@
+const changeColorBtn = document.querySelector(".change-color");
+const spanResult = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+spanResult.textContent = `${getRandomHexColor()}`;
